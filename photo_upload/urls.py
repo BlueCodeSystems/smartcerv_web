24176266975f355fb1bc1f46b15aeda1a0477f6e
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2019-08-26 11:30:50 +0200

    issue #1: redirect home page to admin everytime

--- photo_upload/urls.py
+++ photo_upload/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 app_name = 'photo_upload'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', admin.site.urls),
 ]
